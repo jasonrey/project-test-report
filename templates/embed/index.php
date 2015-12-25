e2f4d9f04e92809ead8927d4087ec458c13219a5
Jason Rey <jasonrey@outlook.com>
2015-12-25 15:06:00 +0800

    Updated interface to have tabs

--- templates/embed/index.php
+++ templates/embed/index.php
@@ -4,33 +4,44 @@
 <a href="javascript:void(0);" id="report-close-button">&times;</a>
 
 <?php if ($isLoggedIn) { ?>
-<div id="report-frame">
-	<form id="report-form">
-		<div class="form-group">
-			<label for="report-screenshots">Screenshots</label>
-			<div id="report-screenshots" name="report-screenshots" class="report-screenshots">
-				<div id="drop-file-mask">
-					<div class="drop-file-message icon-upload">Upload this image</div>
+<div id="report-frame" data-tab="report">
+	<div id="report-tab-navs">
+		<a href="javascript:void(0);" class="report-tab-nav" data-name="report"><i class="icon-feather-paper"></i><p>Report</p></a>
+		<a href="javascript:void(0);" class="report-tab-nav" data-name="inbox"><i class="icon-feather-archive"></i><p>Inbox</p></a>
+	</div>
+
+	<div id="report-tab-contents">
+		<div class="report-tab-content">
+			<form id="report-form">
+				<div class="form-group">
+					<label for="report-screenshots">Screenshots</label>
+					<div id="report-screenshots" name="report-screenshots" class="report-screenshots">
+						<div id="drop-file-mask">
+							<div class="drop-file-message icon-upload">Upload this image</div>
+						</div>
+						<button type="button" id="report-screenshot-add" class="report-screenshot"><i class="icon-plus"></i></button>
+					</div>
+					<input type="file" name="report-screenshot-file" id="report-screenshot-file" />
 				</div>
-				<button type="button" id="report-screenshot-add" class="report-screenshot"><i class="icon-plus"></i></button>
-			</div>
-			<input type="file" name="report-screenshot-file" id="report-screenshot-file" />
+
+				<div class="form-group">
+					<label for="report-text">Report</label>
+					<textarea name="report-text"></textarea>
+				</div>
+
+				<input type="hidden" name="project" value="<?php echo $project; ?>" />
+
+				<button class="form-submit">Submit</button>
+
+				<div id="report-submitting">
+					<div class="report-submitting-message report-submitting-message-loading"><i class="icon-loader icon-spin"></i></div>
+					<div class="report-submitting-message report-submitting-message-completed"><i class="icon-ok"></i></div>
+				</div>
+			</form>
 		</div>
-
-		<div class="form-group">
-			<label for="report-text">Report</label>
-			<textarea name="report-text"></textarea>
+		<div class="report-tab-content">
 		</div>
-
-		<input type="hidden" name="project" value="<?php echo $project; ?>" />
-
-		<button class="form-submit">Submit</button>
-
-		<div id="report-submitting">
-			<div class="report-submitting-message report-submitting-message-loading"><i class="icon-loader icon-spin"></i></div>
-			<div class="report-submitting-message report-submitting-message-completed"><i class="icon-ok"></i></div>
-		</div>
-	</form>
+	</div>
 </div>
 
 <script type="text/html" id="report-screenshot-item">
