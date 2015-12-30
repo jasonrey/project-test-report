74d012f1154f833371ed273c21464a3fc5356b9f
Jason Rey <jasonrey@outlook.com>
2015-12-30 16:57:44 +0800

    For now not going ahead with multiple assignees

--- schemas/report_assignee.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `report_assignee` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `report_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `state` tinyint(1) NOT NULL,
-  `date` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
