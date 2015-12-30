b089a0584b19ed30ba0dff537c1c8d1c6ad90486
Jason Rey <jasonrey@outlook.com>
2015-12-30 18:07:29 +0800

    Added single assignee id to report table

--- schemas/report.sql
+++ schemas/report.sql
@@ -2,6 +2,7 @@ CREATE TABLE `report` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `project_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `assignee_id` int(11) NOT NULL,
   `url` varchar(255) NOT NULL DEFAULT '',
   `content` text NOT NULL,
   `state` tinyint(1) NOT NULL,
--- tables/report.php
+++ tables/report.php
@@ -5,6 +5,7 @@ class ReportTable extends Table
 {
 	public $project_id;
 	public $user_id;
+	public $assignee_id;
 	public $url;
 	public $content;
 	public $state;
