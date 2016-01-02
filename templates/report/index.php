53b69aa21d5c1909b8e9efeecf04a59ff42c9a73
Jason Rey <jasonrey@outlook.com>
2016-01-02 13:41:29 +0800

    Added back button in report item

--- templates/report/index.php
+++ templates/report/index.php
@@ -3,6 +3,7 @@
 ?>
 <?php if ($isLoggedIn) { ?>
 <div id="report-frame">
+	<a class="report-item-back icon-feather-rewind" href="<?php echo Lib::url('index'); ?>">Back</a>
 	<ul id="report-item-list">
 		<?php echo Lib::output('embed/report-item', array('report' => $report, 'user' => $user, 'assignees' => $assignees, 'commentsLoaded' => true)); ?>
 	</ul>
