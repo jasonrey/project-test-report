c3ee0ee91e70f7e0fe9c458b7da6f1b14bdc59ac
Jason Rey <jasonrey@outlook.com>
2016-01-06 14:19:30 +0800

    Added new project screen

--- templates/embed/new-project.php
+++ templates/embed/new-project.php
@@ -2,16 +2,18 @@
 !defined('SERVER_EXEC') && die('No access.');
 ?>
 <div class="new-project">
-	<form class="new-project-content">
+	<form class="new-project-content" method="post" action="<?php echo Lib::url('controller', array('controller' => 'project', 'action' => 'saveProjectTitle')); ?>">
 		<h1>Hi there.</h1>
 		<p>Let's name this project:</p>
 
 		<div class="form-group">
-			<input type="text" class="form-input" placeholder="Project Name" />
+			<input type="text" class="form-input" name="project-title" placeholder="Project Name" />
 		</div>
 
 		<div class="form-actions">
 			<button class="icon-feather-check">OK</button>
 		</div>
+
+		<input type="hidden" name="project-name" value="<?php echo $name; ?>" />
 	</form>
 </div>
